281187cfcdef44f2a032cce931f86f39a47622eb
marciodsousa <marciodesousa16@gmail.com>
2016-10-20 20:59:34 +0100

    #MAS# fixed api ports

--- src/js/actions/forecastActions.js
+++ src/js/actions/forecastActions.js
@@ -4,7 +4,7 @@ export function fetchForecast(input) {
     console.log("Fethcing forecast info for: " + input.location);
     return function(dispatch) {
         dispatch({ type: "FETCH_FORECAST", payload: null })
-        axios.get("http://localhost:8080/public/index.php/forecastlocation/" + input.location.replace(" ", "%20"))
+        axios.get("http://localhost:80/public/index.php/forecastlocation/" + input.location.replace(" ", "%20"))
             .then((response) => {
                 dispatch({ type: "FETCH_FORECAST_FULFILLED", payload: response.data })
             })
