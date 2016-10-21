9f475fcf2e9e94fae02ebaf3bfec182842adf02c
marciodsousa <marciodesousa16@gmail.com>
2016-10-21 08:46:24 +0100

    #MAS# fixed api endpoints, hopefully

--- src/js/actions/forecastActions.js
+++ src/js/actions/forecastActions.js
@@ -4,12 +4,12 @@ export function fetchForecast(input) {
     console.log("Fethcing forecast info for: " + input.location);
     return function(dispatch) {
         dispatch({ type: "FETCH_FORECAST", payload: null })
-        axios.get("http://localhost:80/public/index.php/forecastlocation/" + input.location.replace(" ", "%20"))
+        axios.get("/public/index.php/forecastlocation/" + input.location.replace(" ", "%20"))
             .then((response) => {
                 dispatch({ type: "FETCH_FORECAST_FULFILLED", payload: response.data })
 
                 dispatch({ type: "FETCH_30DAY_FORECAST", payload: null })
-                axios.get("http://localhost:80/public/index.php/forecastlocation/" + input.location.replace(" ", "%20") + "/pastdays/30")
+                axios.get("/public/index.php/forecastlocation/" + input.location.replace(" ", "%20") + "/pastdays/30")
                     .then((response) => {
                         dispatch({ type: "FETCH_30DAY_FORECAST_FULFILLED", payload: response.data })
                     })
